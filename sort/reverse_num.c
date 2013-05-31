761e906114ff14fee5ce4093fd45b2ce7c94eeb3
wangzhengyi <15866883396@126.com>
2013-05-31 11:59:03 +0800

    归并思路ac了求数组逆序对的题目

---
+++ sort/reverse_num.c
@@ -0,0 +1,74 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+long long int count;
+
+/**
+ * 将有二个有序数列a[first...mid]和a[mid...last]合并
+ */
+void merge(int *arr, int first, int mid, int end, int *temp)
+{
+	int i, j, k;
+	i = first;
+	j = mid + 1;
+	k = 0;
+
+	while (i <= mid && j <= end) {
+		if (arr[i] <= arr[j]) {
+			temp[k] = arr[i ++];
+		} else {
+			temp[k] = arr[j ++];
+			count += mid - i + 1;
+		}
+		k ++;
+	}
+
+	while (i <= mid) {
+		temp[k ++] = arr[i ++];
+	}
+
+	while (j <= end) {
+		temp[k ++] = arr[j ++];
+	}
+
+	// 对arr重新赋值，合并
+	for (i = 0; i < k; i ++) 
+		arr[first + i] = temp[i];
+}
+
+/**
+ * 归并排序递归过程
+ */
+void merge_sort(int *arr, int begin, int end, int *temp)
+{
+	int mid;
+	if (begin < end) {
+		mid = (begin + end) / 2;
+		merge_sort(arr, begin, mid, temp);
+		merge_sort(arr, mid + 1, end, temp);
+		merge(arr, begin, mid, end, temp);
+	}
+}
+
+int main()
+{
+	int i, n, *arr, *temp;
+
+	while (scanf("%d", &n) != EOF) {
+		arr = (int *)malloc(sizeof(int) * n);
+		temp = (int *)malloc(sizeof(int) * n);
+		count = 0;
+
+		for (i = 0; i < n; i ++) {
+			scanf("%d", &arr[i]);
+		}
+
+		merge_sort(arr, 0, n - 1, temp);
+
+		printf("%lld\n", count);
+		free(arr);
+		free(temp);
+	}
+
+	return 0;
+}
