3b0961ee8690aa12af6638a6f51c70fd04bf3103
wangzhengyi <15866883396@126.com>
2013-07-10 00:10:54 +0800

    字符串转整数的问题代码

---
+++ string/atoi.c
@@ -0,0 +1,87 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+/**
+ * 判断字符串的合法性
+ *
+ * 1为非法，0为合法
+ *
+ */
+int judgeStrIllegal(char *str)
+{
+	int i, len, flag;
+
+	len = strlen(str);
+
+	if (len == 0) {
+		flag = 1;
+	} else if (len == 1) {
+		if (!(str[0] >= '0' && str[0] <= '9'))
+			flag = 1;
+		else
+			flag = 0;
+	} else {
+		for (i = 0, flag = 0; str[i] != '\0'; i ++) {
+			if (i == 0) {
+				if (str[i] == '-' || str[i] == '+' || (str[i] >= '0' && str[i] <= '9')) {
+					continue;
+				} else {
+					flag = 1;
+					break;
+				}
+			} else {
+				if (str[i] >= '0' && str[i] <= '9') {
+					continue;
+				} else {
+					flag = 1;
+					break;
+				}
+			}
+		}
+	}
+
+	return flag;
+}
+
+int main(void)
+{
+	int i, sign;
+	long long int n;
+	char str[50];
+	while (gets(str)) {
+		// 判断字符串合法性
+		if (judgeStrIllegal(str)) {
+			printf("My God\n");
+			continue;
+		}
+
+		// 将字符串转换为整数
+		for (i = 0, sign = 1; str[i] != '\0'; i ++) {
+			if (i == 0) {
+				switch (str[i]) {
+					case '+' :
+						n = 0;
+						break;
+					case '-' :
+						n = 0;
+						sign = 0;
+						break;
+					default :
+						n = str[i] - '0';
+						break;
+				}
+			} else {
+				n = 10 * n + (str[i] - '0');
+			}
+		}
+
+		if (sign == 0)
+			n *= -1;
+
+		printf("%lld\n", n);
+	}
+
+	return 0;
+}
