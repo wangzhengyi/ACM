563f5dd66fba5733eb84b7949a696b236bea5a4e
wangzhengyi <15866883396@126.com>
2013-09-17 09:27:10 +0800

    提交二分法求整数开方的ac代码

---
+++ number_theory/sqrt.c
@@ -0,0 +1,52 @@
+/**
+ * 创新工厂2014年校招算法题目，求整数N的开方，精度为0.001
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+#define ACCURACY 0.001
+
+double newSqrt(double n)
+{
+	double low, high, mid, tmp;
+
+	// 获取上下界
+	if (n > 1)	{
+		low = 1;
+		high = n;
+	} else {
+		low = n;
+		high = 1;
+	}
+
+	// 二分法求开方
+	while (low <= high) {
+		mid = (low + high) / 2.000;
+
+		tmp = mid * mid;
+
+		if (abs(tmp - n) <= ACCURACY) {
+			return mid;
+		} else if (tmp > n) {
+			high = mid;
+		} else {
+			low = mid;
+		}
+	}
+
+	return -1.000;
+}
+
+int main(void)
+{
+	double n, res;
+
+	while (scanf("%lf", &n) != EOF) {
+		res = newSqrt(n);
+		printf("%lf\n", res);
+	}
+
+	return 0;
+}
